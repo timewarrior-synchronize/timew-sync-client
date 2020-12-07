d8d1373b39c7897e95b8c901a0d72eb71719ee1b
Arne Turuc <arneturuc@gmail.com>
2020-12-07 19:49:50 +0100

    Fix unused imports, formatting and file paths

--- tests/test_io_handler.py
+++ tests/test_io_handler.py
@@ -1,8 +1,6 @@
 import os
-import re
-import pathlib
-from pathlib import Path
 import timewsync.io_handler
+
 
 def test_read_data():
     # write a testfile
@@ -13,5 +11,3 @@ def test_read_data():
     assert ("This is a test.\nAnd this is its second line." in timewsync.io_handler.read_data())
 
     os.remove(os.path.expanduser('~') + "/.timewarrior/data/0000-00.data")
-
-test_read_data()
