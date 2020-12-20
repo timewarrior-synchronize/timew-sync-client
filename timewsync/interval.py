cc37dd647ed7ed7019064127965fc5b2dcad9328
Arne Turuc <arneturuc@gmail.com>
2020-12-20 21:19:16 +0100

    Add Interval functionality - Constructor with/without arguments - To string representation - Parse string to Interval - Tokenizer helper function

--- timewsync/interval.py
+++ timewsync/interval.py
@@ -25,7 +25,114 @@
 ###############################################################################
 
 
+from datetime import datetime
+from typing import List
+
+from dateparser import parse
+
+
 class Interval:
 
-    def __init__(self, start, end, tags, annotation):
-        pass
+    def __init__(self, start: datetime = None, end: datetime = None, tags: List[str] = None, annotation: str = None):
+        self.start: datetime = start
+        self.end: datetime = end
+        self.tags: List[str] = tags
+        self.annotation: str = annotation
+
+    def __str__(self) -> str:
+        """Returns the interval as a string."""
+        out = 'inc'
+        if self.start:
+            out += ' ' + self.start.__str__()
+            if self.end:
+                out += ' - ' + self.end.__str__()
+        if self.tags:
+            out += ' #'
+            for tag in self.tags:
+                out += ' ' + tag
+        if self.annotation:
+            if not self.tags:
+                out += ' #'
+            out += ' # ' + self.annotation
+        return out
+
+
+def as_interval(line: str) -> Interval:
+    """Parses an Interval from the provided string.
+
+    Syntax (tokens separated by whitespace):
+        'inc' [ <iso> [ '-' <iso> ]] [ '#' [ <tag> [ <tag> ... ]] [ '#' <annotation> ]]
+
+    Args:
+        line: An interval string in correct syntax.
+
+    Returns:
+        The same interval parsed to an Interval object.
+    """
+    tokens = tokenize(line)
+
+    # Required 'inc'
+    assert line.startswith('inc')
+    interval = Interval()
+    offset = 0
+
+    # Optional <iso>
+    if len(tokens) > 1 and len(tokens[1]) == 16:
+        interval.start = parse(tokens[1], date_formats=['%Y%m%dT%H%M%SZ'])
+        offset = 1
+
+        # Optional '-' <iso>
+        if len(tokens) > 3 and tokens[2] == '-' and len(tokens[3]) == 16:
+            interval.end = parse(tokens[3], date_formats=['%Y%m%dT%H%M%SZ'])
+            offset = 3
+
+    # Optional '#'
+    if len(tokens) > 2 + offset and tokens[1 + offset] == '#':
+
+        # Optional <tag> ...
+        interval.tags = []
+        index = 2 + offset
+        while index < len(tokens) and tokens[index] != '#':
+            interval.tags.append(tokens[index])
+            index += 1
+
+        # Optional '#' <annotation>
+        if index < len(tokens) and tokens[index] == '#':
+            annotation = ''
+            for i in range(index + 1, len(tokens)):
+                annotation += ' ' + tokens[i]
+            interval.annotation = annotation.lstrip()
+
+    return interval
+
+
+def tokenize(line: str) -> List[str]:
+    """Converts the input string into tokens.
+
+    Tokens are split at any whitespace and in string format.
+    Recognizes double quoted sentences as one token.
+
+    Args:
+        line: The string input to be tokenized.
+
+    Returns:
+        A list of tokens based on the string.
+    """
+    line = ' ' + line + ' '
+    eos = len(line)
+    tokens = []
+    start = 0
+
+    while start < eos:
+        mid = line.find(' "', start)
+        end = line.find('" ', mid+2)
+
+        if mid == -1 or end == -1:
+            tokens += line[start:].split()
+            start = eos
+        else:
+            tokens += line[start:mid].split()
+            tokens += [line[mid+1:end+1]]
+            start = end + 2
+
+    return tokens
