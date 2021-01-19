f90ece4af4c8bc5765c19c8b04f269e8a22ff270
Schmidt, Joachim <joachim.schmidt@stud.tu-darmstadt.de>
2021-01-19 11:04:46 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- timewsync/dispatch.py
+++ timewsync/dispatch.py
@@ -54,7 +54,7 @@ def dispatch(
     """
     diff = generate_diff(timew_intervals, snapshot_intervals)
 
-    request_url = os.path.join(config.base_url, SYNC_ENDPOINT)
+    request_url = os.path.join(config.server_base_url, SYNC_ENDPOINT)
     request_body = json_converter.to_json_request(config.user_id, diff)
 
     server_response = requests.put(request_url, request_body)
