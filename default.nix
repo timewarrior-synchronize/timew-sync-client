b87384dfb8cff6e7360fb9f82b97f501c49d4c28
joachimschmidt557 <joachim.schmidt557@outlook.com>
2021-03-06 22:50:24 +0100

    Update with new dependendy jwcrypto
    See: #27

--- default.nix
+++ default.nix
@@ -11,18 +11,6 @@ buildPythonApplication rec {
 
   propagatedBuildInputs = [
     requests
+    jwcrypto
   ];
-
-  # meta = with pkgs; {
-  #   homepage = "https://ytdl-org.github.io/youtube-dl/";
-  #   description = "Timewarrior Synchronization Client";
-  #   longDescription = ''
-  #     youtube-dl is a small, Python-based command-line program
-  #     to download videos from YouTube.com and a few more sites.
-  #     youtube-dl is released to the public domain, which means
-  #     you can modify it, redistribute it or use it however you like.
-  #   '';
-  #   license = licenses.mit;
-  #   platforms = with platforms; linux ++ darwin;
-  # };
 }
