81270fff847b80c4703cfed86566f96fa4fa63cd
arne.turuc <arne.turuc@stud.tu-darmstadt.de>
2024-02-08 20:09:53 +0100

    Remove output writing to stderr This fixes colors and keeps the order of appearance with other output texts to `stdout`

--- timewsync/__init__.py
+++ timewsync/__init__.py
@@ -186,7 +186,7 @@ def sync(configuration: Configuration) -> None:
 
     # Active time tracking
     if active_interval:
-        print("Time tracking is active. Stopped time tracking to prevent conflicts.", file=sys.stderr)
+        print("Time tracking is active. Stopped time tracking to prevent conflicts.")
 
     # Communicate with server
     try:
@@ -237,7 +237,7 @@ def sync(configuration: Configuration) -> None:
     if active_interval and started_tracking:
         print("Restarted time tracking from the point it was stopped.")
 
-    print("Synchronization successful!", file=sys.stderr)
+    print("Synchronization successful!")
 
     if active_interval and not started_tracking:
         log.warning(
