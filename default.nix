9bbdcabfb5e7b2c76f5db1df21b1b4f3c8dc4b2f
arne.turuc <arne.turuc@stud.tu-darmstadt.de>
2024-04-24 12:26:24 +0200

    Update default.nix requirements & version

--- default.nix
+++ default.nix
@@ -16,17 +16,16 @@ let
 in
 mach-nix.buildPythonApplication {
   pname = "timewsync";
-  version = "1.0.0";
+  version = "1.1";
 
   src = ./.;
 
   format = "pyproject";
 
   requirements = ''
-    requests~=2.25.0
-    colorama~=0.4.4
-    jwcrypto~=0.8
-    python_jwt~=3.3
-    six~=1.9
+    colorama~=0.4.6
+    jwcrypto~=1.5.6
+    pyjwt~=2.8.0
+    requests~=2.31.0
   '';
 }
