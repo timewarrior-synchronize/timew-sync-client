7a06e41a871ff8619933d1602581db63693d14c0
arneturuc <arneturuc@gmail.com>
2024-05-22 16:04:34 +0200

    Use requirements.txt as base for requirements in default.nix
    Co-authored-by: Joachim Schmidt <joachim.schmidt557@outlook.com>

--- default.nix
+++ default.nix
@@ -22,10 +22,5 @@ mach-nix.buildPythonApplication {
 
   format = "pyproject";
 
-  requirements = ''
-    colorama~=0.4.6
-    jwcrypto~=1.5.6
-    pyjwt~=2.8.0
-    requests~=2.31.0
-  '';
+  requirements = builtins.readFile ./requirements.txt;
 }
